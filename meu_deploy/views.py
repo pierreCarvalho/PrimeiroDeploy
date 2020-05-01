b8f5537e422f8d1cef79fff6648bae35d501a876
Pierre Carvalho <pierrefenner@hotmail.com>
2020-05-01 17:00:36 -0300

    meu primeiro template

--- meu_deploy/views.py
+++ meu_deploy/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+
+
+def home(request):
+    # return redirect(reverse('dashboardapp:home'))
+    return render(request, 'inicio.html')
