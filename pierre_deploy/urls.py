afcfce0b228211e5f5352801c125dc1daa0f4586
Pierre Carvalho <pierrefenner@hotmail.com>
2020-05-01 17:02:58 -0300

    meu primeiro template

--- pierre_deploy/urls.py
+++ pierre_deploy/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('meu_deploy.urls')),
